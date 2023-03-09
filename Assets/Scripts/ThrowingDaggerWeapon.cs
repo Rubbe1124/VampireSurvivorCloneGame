2edfbfc797a0aa0790fc915d981411008e421d22
Rubbe1124 <nbj10@naver.com>
2023-03-09 23:41:30 +0900

    17. weapon manager
    추상화와 가상함수를 이용해 공격 방식에 공통으로 들어갈 요소들을 넣어 이후의 무기 스크립트가 상속받도록 기반을 만들었고, 기존의 공격을 담당하는 스크립트가 앞에 만든 스크립트를 상속받도록 함.
    
    기존의 캐릭터 오브젝트의 자식에 있던 무기 오브젝트를 스크립트화 하여 오브젝트 배치가 아닌 스크립터블오브젝트를 이용해 배치하는 방식으로 바꿨고, 스크립터블오브젝트로 시작 무기를 지정할 수 있도록 함. 이후에 무기 추가를 동적으로 할 수 있는 기반을 마련함.

--- Assets/Scripts/ThrowingDaggerWeapon.cs
+++ Assets/Scripts/ThrowingDaggerWeapon.cs
@@ -3,11 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowingDaggerWeapon : MonoBehaviour
+public class ThrowingDaggerWeapon : WeaponBase
 {
-    [SerializeField] float timeToAttack;
-    float timer;
-
     PlayerMove playerMove;
 
     [SerializeField] GameObject knifePrefab;
@@ -17,19 +14,7 @@ public class ThrowingDaggerWeapon : MonoBehaviour
         playerMove = GetComponentInParent<PlayerMove>();
     }
 
-    private void Update()
-    {
-        if(timer<timeToAttack)
-        {
-            timer += Time.deltaTime;
-            return;
-        }
-
-        timer = 0;
-        SpawnKnife();
-    }
-
-    private void SpawnKnife()
+    public override void Attack()
     {
         GameObject thrownKnife = Instantiate(knifePrefab);
         thrownKnife.transform.position = transform.position;
